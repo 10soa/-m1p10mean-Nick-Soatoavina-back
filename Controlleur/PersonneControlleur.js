1e541144220e856bcc9f3254c2edb99c3b5f122d
mathieu29nick <96386431+mathieu29nick@users.noreply.github.com>
2023-01-14 14:07:11 +0200

    commit-Test

--- Controlleur/PersonneControlleur.js
+++ Controlleur/PersonneControlleur.js
@@ -16,7 +16,7 @@ exports.getPersonne = async (req, res) => {
     ])
     res.status(200).json({
       status: 200,
-      data: posts[2].poste[1].poste
+      data: posts[2]
     });
   } catch (err) {
     res.status(400).json({
