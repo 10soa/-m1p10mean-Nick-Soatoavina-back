283d382ca1a21d86693f3d9833a8b1c3dcc08003
mathieu29nick <96386431+mathieu29nick@users.noreply.github.com>
2023-01-14 12:41:13 +0200

    lookUp

--- Controlleur/PersonneControlleur.js
+++ Controlleur/PersonneControlleur.js
@@ -4,10 +4,19 @@ var ObjectID = require('mongoose').Types.ObjectId;
 
 exports.getPersonne = async (req, res) => {
   try {
-    let posts = await Personne.find();
+    let posts = await Personne.aggregate([
+      {
+        $lookup: {
+          from : "Poste",
+          localField : "nom",
+          foreignField : "idPersonne",
+          as : "poste"
+        } 
+      }
+    ])
     res.status(200).json({
       status: 200,
-      data: posts,
+      data: posts[2].poste,
     });
   } catch (err) {
     res.status(400).json({
