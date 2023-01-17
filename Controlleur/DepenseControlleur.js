5dfdc35aa20b854e8695fc1e163e18977a7b2ac2
mathieu29nick <96386431+mathieu29nick@users.noreply.github.com>
2023-01-17 22:12:41 +0200

    crud-Depense

--- Controlleur/DepenseControlleur.js
+++ Controlleur/DepenseControlleur.js
@@ -18,7 +18,7 @@ exports.getDepenses = async (req, res) => {
 };
 
 exports.getDepense = async (req, res) => {
-  Depense.findOne({ depense_id: req.params.depense_id })
+  Depense.findOne({ _id: req.params.depense_id })
   .then(data => res.status(200).json({ data }))
   .catch((error) => res.status(404).json({msg: error}))
 };
@@ -30,13 +30,13 @@ exports.createDepense = async (req, res) => {
 };
 
 exports.updateDepense = async (req, res) => {
-  Depense.findOneAndUpdate({ depense_id: req.params.depense_id }, req.body, { new: true, runValidators: true })
+  Depense.findOneAndUpdate({ _id: req.params.depense_id }, req.body, { new: true, runValidators: true })
   .then(result => res.status(200).json({ result }))
   .catch((error) => res.status(404).json({msg: error }))
 };
 
 exports.deleteDepense = async (req, res) => {
-  Depense.findOneAndDelete({ depense_id: req.params.depense_id })
+  Depense.findOneAndDelete({ _id: req.params.depense_id })
   .then(result => res.status(200).json({ result }))
   .catch((error) => res.status(404).json({msg: error }))
 };
