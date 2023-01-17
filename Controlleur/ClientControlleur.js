5eb775f76e11a043b51a562df250e1cd1619fd0d
mathieu29nick <96386431+mathieu29nick@users.noreply.github.com>
2023-01-17 20:32:31 +0200

    crud-Client

--- Controlleur/ClientControlleur.js
+++ Controlleur/ClientControlleur.js
@@ -2,17 +2,41 @@
 var { Client } = require('../Model/ClientModel');
 var ObjectID = require('mongoose').Types.ObjectId;
 
-exports.getClient = async (req, res) => {
-  try {
-    let data = await Client.find();
-    res.status(200).json({
-      status: 200,
-      data: data
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 400,
-      message: err.message,
-    });
-  }
+exports.getClients = async (req, res) => {
+    try {
+        let data = await Client.find();
+        res.status(200).json({
+        status: 200,
+        data: data
+        });
+    }catch (err) {
+        res.status(400).json({
+        status: 400,
+        message: err.message,
+        });
+    }
+};
+
+exports.getClient = ((req, res) => {
+    Client.findOne({ client_id: req.params.client_id })
+    .then(data => res.status(200).json({ data }))
+    .catch((error) => res.status(404).json({msg: error}))
+});
+
+exports.createClient = async (req, res) => {
+    Client.create(req.body)
+    .then(result => res.status(200).json({ result }))
+    .catch((error) => res.status(500).json({msg:  error }))
+};
+
+exports.updateClient = async (req, res) => {
+    Client.findOneAndUpdate({ client_id: req.params.client_id }, req.body, { new: true, runValidators: true })
+    .then(result => res.status(200).json({ result }))
+    .catch((error) => res.status(404).json({msg: error }))
+};
+
+exports.deleteClient = async (req, res) => {
+    Client.findOneAndDelete({ client_id: req.params.client_id })
+    .then(result => res.status(200).json({ result }))
+    .catch((error) => res.status(404).json({msg: error }))
 };
--- routes/Client.js
+++ routes/Client.js
@@ -4,6 +4,10 @@ const router = express.Router();
 // Require controller modules.
 const clientControlleur = require('../Controlleur/ClientControlleur')
  
-router.get('/', clientControlleur.getClient);
+router.get('/', clientControlleur.getClients);
+router.get('/:client_id', clientControlleur.getClient);
+router.post('/', clientControlleur.createClient);
+router.put('/:client_id', clientControlleur.updateClient);
+router.delete('/:client_id', clientControlleur.deleteClient);
  
 module.exports = router;
