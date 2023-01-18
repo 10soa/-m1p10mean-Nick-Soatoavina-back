cab71e185355200c6ac4d366ea4767444b327db0
mathieu29nick <96386431+mathieu29nick@users.noreply.github.com>
2023-01-18 20:57:21 +0200

    Inscription-Mail

--- Controlleur/ClientControlleur.js
+++ Controlleur/ClientControlleur.js
@@ -3,6 +3,7 @@ var { Client } = require('../Model/ClientModel');
 var ObjectID = require('mongoose').Types.ObjectId;
 var nodemailer = require('nodemailer');
 let smtpTransport = require('nodemailer-smtp-transport');
+const { Console } = require('console');
 
 exports.getClients = async (req, res) => {
     try {
@@ -43,28 +44,39 @@ exports.deleteClient = async (req, res) => {
     .catch((error) => res.status(404).json({msg: error }))
 };
 
-/* Evoie Mail */
-exports.sendMail =async (req,res) =>{
-    var mail = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'soarabe1234@gmail.com',
-          pass: 'xtypmdgsbwprqjiy'
-        }
-    });
-    var mailOptions = {
-        from: 'soarabe1234@gmail.com',
-        to: 'soaornella5@gmail.com',
-        subject: 'Test Inscription',
-        html:
-        '<p>Please click on the following link to verify your email address:</p>' +
-        '<a href="http://localhost:2002/Mean_projet/Client/"><button>Liste Clinet<button></a>',
-    };
-    mail.sendMail(mailOptions, function(error, info){
-        if (error) {
-          res.send(error);
-        } else {
-          res.send(info.response);
-        }
-    });
+/* Inscription Client */
+exports.inscriptionClient= async (req,res) => {
+    Client.create(req.body)
+    .then(data => {
+        var mail = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: 'soarabe1234@gmail.com',
+              pass: 'xtypmdgsbwprqjiy'
+            }
+        });
+        var mailOptions = {
+            from: 'soarabe1234@gmail.com',
+            to: req.body.mail,
+            subject: 'Inscription sur Reparation_Voiture',
+            html:
+            '<p>'+req.body.mail+' : Verification de votre compte G-mail.</p>' +
+            '<a href=#"><button style="background-color: rgb(100,148,44);border: none;color: white;padding: 15px 70px;text-align: center;text-decoration: none;display: inline-block;font-size: 16px;margin: 4px 2px;cursor: pointer;"> Cliquez ici pour verifier!</button></a>',
+        };
+        mail.sendMail(mailOptions, function(error, info){
+            if (error) {
+              res.send(error);
+            } else {
+              res.send(info.response);
+            }
+        });
+    })
+    .catch((error) => res.status(500).json({msg:  error }))
+};
+
+/* validation compte du client */
+exports.validerCompteClient = async (req, res) => {
+    Client.findOneAndUpdate({ client_id: req.params.client_id }, {valider : 1 }, { new: true, runValidators: true })
+    .then(result => res.status(200).json({ result }))
+    .catch((error) => res.status(404).json({msg: error }))
 };
