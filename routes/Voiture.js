7ee17b687a7619ec0856d1f3b9c6f64b5961406e
mathieu29nick <96386431+mathieu29nick@users.noreply.github.com>
2023-01-30 15:06:27 +0200

    repere

--- routes/Voiture.js
+++ routes/Voiture.js
@@ -43,5 +43,6 @@ router.get("/statistique/benefice", voitureControlleur.benefice);
 router.get("/statistique/chiffreAffaire", voitureControlleur.chiffreAffaire);
 router.get("/listeReparationVoiture1/:client_id/:dateDepos", voitureControlleur.listeReparationVoiture1);
 router.get("/reparationAvancementClient/:client_id", voitureControlleur.listeReparationClient);
+//router.get("/reparationAvancementClient/:client_id", voitureControlleur.listeReparationClient);
 
 module.exports = router;
